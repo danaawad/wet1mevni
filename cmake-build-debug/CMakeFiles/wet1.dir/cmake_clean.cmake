b54730fb6e4cbc9aff2e3a07a02304e402bdc586
danaawad <103368695+danaawad@users.noreply.github.com>
2023-05-23 21:39:46 +0300

    danaaa

---
+++ cmake-build-debug/CMakeFiles/wet1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/wet1.dir/StreamingDBa1.cpp.o"
+  "CMakeFiles/wet1.dir/main.cpp.o"
+  "wet1"
+  "wet1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/wet1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
